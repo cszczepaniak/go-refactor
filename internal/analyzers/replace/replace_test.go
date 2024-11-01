d0f4e11c213d5f83062b4876fc94732be10f8420
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-10-31 22:15:12 -0500

    fix tests

--- internal/analyzers/replace/replace_test.go
+++ internal/analyzers/replace/replace_test.go
@@ -3,28 +3,21 @@ package replace
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func TestReplace_Basic(t *testing.T) {
-	a, err := New(Config{
-		Package:     "test.com/module/basic",
-		Function:    "ReplaceMe",
-		Replacement: "ExampleReplacement",
-	})
-	require.NoError(t, err)
+	a := New()
+	a.Flags.Set("func", "test.com/module/basic.ReplaceMe")
+	a.Flags.Set("replacement", "ExampleReplacement")
 
 	analysistest.RunWithSuggestedFixes(t, analysistest.TestData(), a, "./basic")
 }
 
 func TestReplace_Args(t *testing.T) {
-	a, err := New(Config{
-		Package:     "test.com/module/args",
-		Function:    "ReplaceMe",
-		Replacement: "Replaced($arg1, $arg0)",
-	})
-	require.NoError(t, err)
+	a := New()
+	a.Flags.Set("func", "test.com/module/args.ReplaceMe")
+	a.Flags.Set("replacement", "Replaced($arg1, $arg0)")
 
 	analysistest.RunWithSuggestedFixes(t, analysistest.TestData(), a, "./args")
 }
